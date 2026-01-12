ec251959105fb88ad84238b2391f55b9a18274a4
dondycles <johnrod.dondoyano@gmail.com>
2026-01-13 01:35:06 +0800

    suspense

--- app/edit-money/page.tsx
+++ app/edit-money/page.tsx
@@ -1,44 +1,24 @@
-"use client";
+import { Info } from "lucide-react";
+import EditMoneyComponent from "./_component";
+import { Suspense } from "react";
 
-import EditMoneyForm from "@/components/edit-money-form";
-import { Button } from "@/components/ui/button";
-import { useMoneysStore } from "@/store/MoneysStore";
-import { ChevronLeft, Info } from "lucide-react";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-
-export default function EditMoneyPage() {
-  const searchParams = useSearchParams();
-  const targetMoneyId = searchParams.get("moneyId");
-  const { moneys } = useMoneysStore();
-  const targetMoney = moneys.find((money) => money.id === targetMoneyId);
-
-  if (!targetMoney)
+export default async function EditMoneyPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ moneyId?: string }>;
+}) {
+  const targetMoneyId = (await searchParams).moneyId;
+  if (!targetMoneyId)
     return (
       <main className="flex min-h-screen w-full flex-col items-center justify-center pt-4 pb-32 px-4 mx-auto gap-6 max-w-lg">
         <Info className="size-16" />
-        <h1 className="text-foreground text-2xl font-black">
-          Money Not Existing
-        </h1>
+        <h1 className="text-foreground text-2xl font-black">No Money ID</h1>
       </main>
     );
 
   return (
-    <main className="flex min-h-screen w-full flex-col items-center pt-4 pb-32 px-4 mx-auto gap-6 max-w-lg">
-      <div className="flex flex-wrap gap-4 justify-between w-full border-b p-4">
-        <Button size="icon" variant="secondary" asChild>
-          <Link href="/list">
-            <ChevronLeft />
-          </Link>
-        </Button>
-        <div className="text-right">
-          <h1 className="text-foreground text-2xl font-black">Edit Money</h1>
-          <p className="text-muted-foreground font-semibold">
-            Edit it as you want.
-          </p>
-        </div>
-      </div>
-      <EditMoneyForm money={targetMoney} />
-    </main>
+    <Suspense>
+      <EditMoneyComponent targetMoneyId={targetMoneyId} />
+    </Suspense>
   );
 }
