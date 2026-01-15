94f155b53dce46d56f15bcb653621d1c54b26cfd
dondycles <johnrod.dondoyano@gmail.com>
2026-01-16 01:08:31 +0800

    now with add reason

--- components/action-alert-dialog.tsx
+++ components/action-alert-dialog.tsx
@@ -18,10 +18,16 @@ import { useRouter } from "next/navigation";
 import _ from "lodash";
 import { useHistoryStore } from "@/store/History";
 import { nanoid } from "nanoid";
+import { useState } from "react";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import Loader from "./loader";
 export default function ActionAlertDialog() {
   const router = useRouter();
   const { remove, edit, moneys } = useMoneysStore();
   const { addHistory } = useHistoryStore();
+  const [reason, setReason] = useState<string | undefined>();
+
   const {
     openDialog,
     moneyInAction,
@@ -40,12 +46,19 @@ export default function ActionAlertDialog() {
     setMoneyInAction(undefined);
     setMoneyInActionNewData(undefined);
     setTypeOfAction(undefined);
+    setReason(undefined);
     setOpenDialog(false);
   };
 
-  if (!moneyInAction) return null;
+  // if (!moneyInAction) return null;
   return (
-    <AlertDialog open={openDialog} onOpenChange={setOpenDialog}>
+    <AlertDialog
+      open={openDialog}
+      onOpenChange={(state) => {
+        if (!state) reset();
+        setOpenDialog(state);
+      }}
+    >
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
@@ -53,68 +66,82 @@ export default function ActionAlertDialog() {
             This action cannot be undone.
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <div className="flex flex-col gap-2">
-          {moneyInAction ? (
-            <MoneyCard withOptions={false} money={moneyInAction} />
-          ) : null}
-          {moneyInActionNewData ? (
-            <>
-              <ArrowDown className="mx-auto text-muted-foreground size-5" />
-              <MoneyCard withOptions={false} money={moneyInActionNewData} />
-            </>
-          ) : null}
-        </div>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <Button asChild variant="destructive">
-            <AlertDialogAction
-              onClick={() => {
-                if (typeOfAction === "removeMoney") {
-                  remove(moneyInAction);
-                  addHistory({
-                    date_added: date,
-                    id: nanoid(),
-                    money_id: moneyInAction.id,
-                    type: "add",
-                    snapshot: {
-                      before: { money: moneyInAction, total_money },
-                      after: {
-                        money: { ...moneyInAction, amount: 0 },
-                        total_money:
-                          Number(total_money) - Number(moneyInAction.amount),
-                      },
-                    },
-                  });
-                  reset();
-                }
-                if (typeOfAction === "editMoney") {
-                  if (!moneyInActionNewData) return reset();
-                  edit(moneyInActionNewData);
-                  addHistory({
-                    date_added: date,
-                    id: nanoid(),
-                    money_id: moneyInAction.id,
-                    type: "add",
-                    snapshot: {
-                      before: { money: moneyInAction, total_money },
-                      after: {
-                        money: moneyInActionNewData,
-                        total_money:
-                          Number(total_money) -
-                          Number(moneyInAction.amount) +
-                          Number(moneyInActionNewData.amount),
-                      },
-                    },
-                  });
-                  reset();
-                  router.push("/list");
-                }
-              }}
-            >
-              Confirm
-            </AlertDialogAction>
-          </Button>
-        </AlertDialogFooter>
+        {moneyInAction ? (
+          <>
+            <div className="flex flex-col gap-2">
+              {moneyInAction ? (
+                <MoneyCard withOptions={false} money={moneyInAction} />
+              ) : null}
+              {moneyInActionNewData ? (
+                <>
+                  <ArrowDown className="mx-auto text-muted-foreground size-5" />
+                  <MoneyCard withOptions={false} money={moneyInActionNewData} />
+                  <Textarea
+                    value={reason}
+                    onChange={(v) => setReason(v.currentTarget.value)}
+                    className="mt-4"
+                    placeholder="Reason of modification (Optional but helpful soon)"
+                  />
+                </>
+              ) : null}
+            </div>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button asChild variant="destructive">
+                <AlertDialogAction
+                  onClick={() => {
+                    if (typeOfAction === "removeMoney") {
+                      remove(moneyInAction);
+                      addHistory({
+                        date_added: date,
+                        id: nanoid(),
+                        money_id: moneyInAction.id,
+                        type: "add",
+                        snapshot: {
+                          before: { money: moneyInAction, total_money },
+                          after: {
+                            money: { ...moneyInAction, amount: 0 },
+                            total_money:
+                              Number(total_money) -
+                              Number(moneyInAction.amount),
+                          },
+                        },
+                      });
+                      reset();
+                    }
+                    if (typeOfAction === "editMoney") {
+                      if (!moneyInActionNewData) return reset();
+                      edit(moneyInActionNewData);
+                      addHistory({
+                        date_added: date,
+                        id: nanoid(),
+                        money_id: moneyInAction.id,
+                        type: "add",
+                        snapshot: {
+                          before: { money: moneyInAction, total_money },
+                          after: {
+                            money: moneyInActionNewData,
+                            total_money:
+                              Number(total_money) -
+                              Number(moneyInAction.amount) +
+                              Number(moneyInActionNewData.amount),
+                          },
+                        },
+                        reason,
+                      });
+                      reset();
+                      router.push("/list");
+                    }
+                  }}
+                >
+                  Confirm
+                </AlertDialogAction>
+              </Button>
+            </AlertDialogFooter>
+          </>
+        ) : (
+          <Loader />
+        )}
       </AlertDialogContent>
     </AlertDialog>
   );
