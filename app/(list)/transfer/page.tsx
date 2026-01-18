6d9b1f61ab2e68653727e3690af3854a74a4ef0b
dondycles <johnrod.dondoyano@gmail.com>
2026-01-19 00:50:25 +0800

    hehe

---
+++ app/(list)/transfer/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Transfer Page",
+};
+
+export default async function TransferPage() {
+  return (
+    <div>
+      <h1>Transfer Page</h1>
+    </div>
+  );
+}
