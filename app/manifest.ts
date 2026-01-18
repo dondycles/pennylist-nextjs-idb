0ecc4058f44dcb5b1adf0c9741734d73d8f452d1
dondycles <johnrod.dondoyano@gmail.com>
2026-01-19 00:01:47 +0800

    vs code

---
+++ app/manifest.ts
@@ -0,0 +1,32 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Pennylist",
+    short_name: "Pennylist",
+    description: "List every pennies you have on Pennylist.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    orientation: "portrait",
+    icons: [
+      {
+        src: "/icons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
