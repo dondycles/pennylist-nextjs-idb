c1111ca2f1b3da5d45094832335dbbb0cc4c3163
dondycles <johnrod.dondoyano@gmail.com>
2026-01-18 22:43:34 +0800

    zed

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,5 +1,4 @@
 import { Loader2 } from "lucide-react";
-
 export default function Loader() {
   return (
     <div className="animate-pulse flex gap-2 items-center text-base text-muted-foreground font-black h-fit">
