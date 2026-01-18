ffe5b0d2293728741b3e1f6a0b30acb99f3c9f61
dondycles <johnrod.dondoyano@gmail.com>
2026-01-19 00:38:06 +0800

    favicon

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,6 +12,11 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait",
     icons: [
       {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
         src: "/icons/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
@@ -21,11 +26,6 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
-      },
-      {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
       },
     ],
   };
